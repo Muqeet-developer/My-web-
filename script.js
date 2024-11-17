1665b0abc03fff4db4ddff4f244dbe3f37e7d4cb
Muqeet-coder <rahilashahzad.7@gmail.com>
2024-11-17 17:09:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const audioElement = document.getElementById('myAudio1');
+const audio = document.getElementById('myAudio2');
+const audio3 = document.getElementById('myAudio3');
+const audio4 = document.getElementById('myAudio4');
+const audio5 = document.getElementById('myAudio5');
+const audio6 = document.getElementById('myAudio6');
+function playAudio1() {
+    audioElement.play();
+}
+function playAudio2() {
+    audio.play();
+}
+function playAudio3() {
+    audio3.play();
+}
+function playAudio4() {
+    audio4.play();
+}
+function playAudio5() {
+    audio5.play();
+}
+function playAudio6() {
+    audio6.play();
+}
